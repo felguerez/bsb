25161435ac747aac777773566bceef69466983dd
Antonio Felguerez <agfelgue@gmail.com>
2019-04-10 08:52:48 -0400

    simplify media controls

--- client/src/media-controls.js
+++ client/src/media-controls.js
@@ -1,27 +1,24 @@
 import React, { forwardRef } from "react";
 
 function MediaControls({ playing, setCurrentTrack, tracks }, ref) {
+
+  const goBack = () => setCurrentTrack(track => !track ? tracks.length - 1: track - 1);
+  const goForward = () => setCurrentTrack(track => track >= tracks.length - 1 ? 0 : track + 1);
+  const togglePlayer = (action) => () => ref.current[action]();
+
   return (
     <div className="media">
       <i
         className="fa fa-backward"
-        onClick={() => setCurrentTrack(track => !track ? tracks.length - 1: track - 1)}
+        onClick={goBack}
       />
-      {playing ? (
-        <i
-          className="fa fa-pause"
-          onClick={() => {
-            ref.current.pause();
-          }} />
-      ) : (
-        <i
-          className="fa fa-play" onClick={() => {
-            ref.current.play();
-          }}/>
-        )}
+      <i
+        className={`fa fa-${playing ? 'pause' : 'play'}`}
+        onClick={togglePlayer(playing ? 'pause' : 'play')}
+      />
       <i
         className="fa fa-forward"
-        onClick={() => setCurrentTrack(track => track >= tracks.length - 1 ? 0 : track + 1)}
+        onClick={goForward}
       />
     </div>
   );
